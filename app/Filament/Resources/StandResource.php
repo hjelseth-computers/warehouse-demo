7303077a1b33b04bafbdd65b4243e630b9964a64
Tommy Evertsen <tommyevertsen52@gmail.com>
2024-04-11 09:26:56 +0200

    Made resources

---
+++ app/Filament/Resources/StandResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StandResource\Pages;
+use App\Filament\Resources\StandResource\RelationManagers;
+use App\Models\Stand;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StandResource extends Resource
+{
+    protected static ?string $model = Stand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = 'Reoler';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->label(__('Navn'))
+                    ->required()
+                    ->maxLength(100),
+
+                TextInput::make('columns')->label(__('Kolonner'))
+                    ->numeric()
+                    ->minValue(1)
+                    ->step(1)
+                    ->required(),
+
+                TextInput::make('rows')->label(__('Rader'))
+                    ->numeric()
+                    ->minValue(1)
+                    ->step(1)
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Navn')->searchable(),
+                TextColumn::make('columns')->label('Kolonner')->searchable(),
+                TextColumn::make('rows')->label('Rader')->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()->requiresConfirmation(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStands::route('/'),
+            'create' => Pages\CreateStand::route('/create'),
+            'edit' => Pages\EditStand::route('/{record}/edit'),
+        ];
+    }
+}
