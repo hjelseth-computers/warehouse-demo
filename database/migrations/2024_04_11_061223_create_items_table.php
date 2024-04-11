055acfe64496e51f7191c64bde6d3693037a78a1
Tommy Evertsen <tommyevertsen52@gmail.com>
2024-04-11 08:42:28 +0200

    Made migrations and models

---
+++ database/migrations/2024_04_11_061223_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Stand;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('type')->nullable();
+            $table->integer('row_placement');
+            $table->integer('column_placement');
+            $table->decimal('depth', 4, 1)->nullable();
+            $table->decimal('diameter', 4, 1)->nullable();
+            $table->foreignId(Stand::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
